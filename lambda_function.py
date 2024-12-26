8dbe0eaaae976cade196ba2f1d8523997351b192
Deon Rennie <trillology47@gmail.com>
2024-12-26 02:00:17 +0000

    updated

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,13 @@
 import requests
 
 def lambda_handler(event, context):
-    url = "https://programming-quotes-api.herokuapp.com/quotes/random"
+    url = "https://zenquotes.io/api/random"  # Alternative URL
     response = requests.get(url)
 
     if response.status_code == 200:
         quote_data = response.json()
-        content = quote_data.get("en")  # The quote text is stored in 'en'
-        author = quote_data.get("author")  # The author is stored in 'author'
+        content = quote_data[0].get("q")  # ZenQuotes stores the quote in 'q'
+        author = quote_data[0].get("a")  # ZenQuotes stores the author in 'a'
         print(f"Random Quote: '{content}' - {author}")
     else:
         print(f"Failed to retrieve a random quote., Status code: {response.status_code}")
