d8cda3fdaff6bb0cf0f3056b81870328a871f915
Deon Rennie <trillology47@gmail.com>
2024-12-26 01:57:52 +0000

    update lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,15 +1,13 @@
 import requests
-import certifi
 
 def lambda_handler(event, context):
-    url = "https://api.quotable.io/random"
+    url = "https://programming-quotes-api.herokuapp.com/quotes/random"
     response = requests.get(url)
 
     if response.status_code == 200:
         quote_data = response.json()
-        content = quote_data.get("content")
-        author = quote_data.get("author")
+        content = quote_data.get("en")  # The quote text is stored in 'en'
+        author = quote_data.get("author")  # The author is stored in 'author'
         print(f"Random Quote: '{content}' - {author}")
-        print("v1.1")
     else:
         print(f"Failed to retrieve a random quote., Status code: {response.status_code}")
